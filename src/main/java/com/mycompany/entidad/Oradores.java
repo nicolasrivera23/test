d28e147596ef86be820d98271d0512505ee7becd
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2023-11-08 16:28:22 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/entidad/Oradores.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.entidad;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Oradores {
+    
+    //atributos de los oradores
+    long id = 0;
+    String nombre = "";
+    String apellido = "";
+    String email = "";
+    String tema = "";
+    
+    //constructor
+
+    public Oradores(String n, String a, String e, String t) {
+        this.nombre = n;
+        this.apellido = a;
+        this.email = e;
+        this.tema = t;
+    }
+    
+    //toString()
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.test;
+
+import com.mycompany.entidad.Oradores;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Test {
+    public static void main(String[] args) {
+        
+        //Declaro distintas variables
+        int numero1 = 1233547897;
+        
+        byte numero2 = (byte)256;
+        
+        long numero3 = 123354789753L;
+        
+        float numero4 = 3.14F;
+        
+        double numero5 = 3.434D;
+        
+        String palabra = "Hola";
+        
+        char letra = 'c';
+        
+        boolean isVerdadero = true;
+        
+        System.out.println("Hello World!" + "2");
+        
+        
+        //Crear objeto oradores
+        Oradores ora1 = new Oradores("Steve", "Jobs", "manzana@gmail.com", "AWS LEX");
+        
+        System.out.println(ora1);
+        
+    }
+    
+    //int numero = "hola";
+    
+    
+    
+
+}
